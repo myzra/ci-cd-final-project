8b551bae1c4f9efdea6af90ebeca261922035511
myzra <myzrahfortnite@gmail.com>
2025-07-21 16:22:56 -0400

    refactored

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -32,7 +32,8 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+            status=status.HTTP_400_BAD_REQUEST,
+            error="Bad Request", message=message
         ),
         status.HTTP_400_BAD_REQUEST,
     )
@@ -43,10 +44,13 @@ def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
+    response = jsonify(
+        status=status.HTTP_404_NOT_FOUND,
+        error="Not Found",
+        message=message
     )
+
+    return response, status.HTTP_404_NOT_FOUND
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
